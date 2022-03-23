de6ac68331118be22a226b5c9297d44da11661c4
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-03-24 00:11:57 +0530

    Time: 4 ms (27.86%), Space: 5.9 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int ans = 0;
+        while(startValue < target){
+            if(target & 1){
+                target++;
+            }
+            else{
+                target /= 2;
+            }
+            ans++;
+        }
+        return ans + (startValue - target);
+    }
+};
