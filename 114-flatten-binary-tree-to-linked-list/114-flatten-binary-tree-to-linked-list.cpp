574c7997caeafcf604ddb980f33dcb525c23ed00
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-04-24 00:43:21 +0530

    Time: 17 ms (8.71%), Space: 13.2 MB (7.84%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -18,23 +18,6 @@ public:
         preorder(root -> left,res);
         preorder(root -> right,res);
     }
-//     void display(TreeNode* root){
-//         if(!root) return;
-        
-//         while(root -> right != NULL || root -> left != NULL){
-//             if(root -> left != NULL){
-//                 cout << root -> left -> val << ','; 
-//             }
-//             if(root -> right != NULL){
-//                 cout << root -> right -> val << ',';
-//             }
-//             else {
-//                 cout << "null,";
-//             }
-            
-//             root = root -> right;
-//         }
-//     }
     void flatten(TreeNode* root) {
         if(!root) return;
         vector<TreeNode*> arr;
@@ -45,33 +28,6 @@ public:
             prev -> left = NULL;
             prev -> right = arr[i+1];
             prev = arr[i+1];
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // for(auto x: inorderArr){
-        //     cout << x << " ";
-        // }
-        // root = new TreeNode(inorderArr[0]);
-        // TreeNode* prev = root;
-        // for(int i = 1; i < inorderArr.size(); i++){
-        //     TreeNode* dummyNode = new TreeNode();
-        //     // dummyNode -> left = NULL;
-        //     dummyNode -> val = inorderArr[i];
-        //     prev -> right = dummyNode;
-        //     prev = dummyNode;
-        // }
-        // // display(root);
-        // cout << root -> right -> right-> right -> val;
-        
+        }        
     }
 };
