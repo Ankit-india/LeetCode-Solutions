4e0d2d28fc95ba2e34545898509a94c02ee96d25
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-05-31 23:13:07 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -8,33 +8,32 @@ class Solution{
 public:
 	void merge(int arr1[], int arr2[], int n, int m) {
 	    // code here
-	    int gap =(int) ceil((n+m)/2.0);
-       while(gap>0)
+	    int gap = (int)ceil((n+m)/2.0);
+       while(gap > 0)
        {
            int i = 0;
            int j = gap;
-           while(j<(n+m)){
-               if(j<n&&arr1[i]>arr1[j]){
+           while(j < (n + m)){
+               if(j < n && arr1[i] > arr1[j]){
                    int temp = arr1[i];
-                   arr1[i]=arr1[j];
-                   arr1[j]=temp;
+                   arr1[i] = arr1[j];
+                   arr1[j] = temp;
                }
-               else if(j>=n&&i<n&&arr1[i]>arr2[j-n]){
+               else if(j >= n && i < n && arr1[i] > arr2[j - n]){
                    int temp = arr1[i];
-                   arr1[i]=arr2[j-n];
-                   arr2[j-n]=temp;
+                   arr1[i] = arr2[j - n];
+                   arr2[j - n] = temp;
                }
-               else if(j>=n&&i>=n&&arr2[i-n]>arr2[j-n]){
-                   int temp = arr2[i-n];
-                   arr2[i-n]=arr2[j-n];
-                   arr2[j-n]=temp;
+               else if(j >= n && i >= n && arr2[i - n] > arr2[j - n]){
+                   int temp = arr2[i - n];
+                   arr2[i-n] = arr2[j - n];
+                   arr2[j-n] = temp;
                }
                i++;
                j++;
-               
            }
-           if(gap==1){
-               gap=0;
+           if(gap == 1){
+               gap = 0;
            }
            else{
                gap = (int)ceil(gap/2.0);
