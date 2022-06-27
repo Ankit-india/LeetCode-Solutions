e2f7996b134fb544d455ba0a89c1cf318f1ac549
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-06-27 08:17:43 +0530

    Time: 4 ms (27.82%), Space: 5.9 MB (90.16%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -7,7 +7,14 @@ public:
         return dp[i][j] = countPath(i+1,j,m,n,dp) + countPath(i,j+1, m,n,dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
-        return countPath(0,0,m,n,dp);
+        // vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        // return countPath(0,0,m,n,dp);
+        int temp = m + n - 2;
+        int temp2 = m-1;
+        double ans = 1;
+        for(int i = 1; i <= temp2; i++){
+            ans = ans * (temp - temp2 + i)/ i;
+        }
+        return (int) ans;
     }
 };
