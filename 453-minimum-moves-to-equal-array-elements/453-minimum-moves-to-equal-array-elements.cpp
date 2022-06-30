80cd8b7bb8be577fec347c0f83d460139527c35f
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-06-30 10:05:12 +0530

    Time: 82 ms (18.69%), Space: 28.4 MB (41.83%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        int n = nums.size();
+        
+        // int n = nums.size();
+        // int ans = 0;
+        // sort(nums.begin(), nums.end());    // O(NlogN)
+        // // int temp = nums[n / 2];
+        // for(int i = 1; i < n; i++){
+        //     ans += abs(nums[0] - nums[i]);
+        // }
+        // // if(ans != 0)return ans+1;
+        // return ans;
+        
+        // O(N)
         int ans = 0;
-        sort(nums.begin(), nums.end());
-        // int temp = nums[n / 2];
-        for(int i = 1; i < n; i++){
-            ans += abs(nums[0] - nums[i]);
+        int n = nums.size();
+        int mn = (1e9 + 8), idx = 0;
+        for(int i = 0; i < n; i++){
+            if(mn > nums[i]){
+                mn = nums[i];
+                idx = i;
+            }
         }
-        // if(ans != 0)return ans+1;
+        for(int i = 0; i < n; i++){
+            if(i != idx){
+                ans+=(nums[i]-mn);
+            }
+        }
         return ans;
     }
 };
