25938b670dff0b4de6ab2a9215d3f991a388d8b4
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-07-21 09:09:04 +0530

    Time: 7 ms (21.51%), Space: 7.4 MB (93.11%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+
+        // Empty list
+        if (head == NULL) {
+            return NULL;
+        }
+
+        // Move the two pointers until they reach the proper starting point
+        // in the list.
+        ListNode* cur = head, *prev = NULL;
+        while (m > 1) {
+            prev = cur;
+            cur = cur->next;
+            m--;
+            n--;
+        }
+
+        // The two pointers that will fix the final connections.
+        ListNode* con = prev, *tail = cur;
+
+        // Iteratively reverse the nodes until n becomes 0.
+        ListNode* third = NULL;
+        while (n > 0) {
+            third = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = third;
+            n--;
+        }
+
+        // Adjust the final connections as explained in the algorithm
+        if (con != NULL) {
+            con->next = prev;
+        } else {
+            head = prev;
+        }
+
+        tail->next = cur;
+        return head;
+    }
+};
