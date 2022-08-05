620d143db2a9e0cabfa993ec15f61b6d8f46501b
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-08-06 02:22:32 +0530

    Time: 3 ms (62.90%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -17,11 +17,35 @@ class Solution {
             ans += mem(nums, target - x, dp);
         }
         return dp[target] = ans;
+        
+    }
+    int tab(vector<int>& nums, int target) {
+//         vector<int>dp(target+1, 0);
+//         dp[0]=1;
+//         for(int i = 1; i <= target; i++){
+//             for(int j = 0; j < nums.size(); j++){
+//                 if(nums[j] <= i){
+//                     dp[i] += dp[i-nums[j]];
+//                 }
+//             }
+//         }
+        
+//         return dp[target];
+         vector< unsigned int> dp(target+1);
+        dp[0]=1;
+        
+        for(int i=1; i<target+1; i++)
+            for(int j=0; j<nums.size(); j++)
+                if(nums[j]<=i)
+                    dp[i] += dp[i-nums[j]];
+        
+        return dp[target];
     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
         // return helperFun(nums, target);
-        vector<int> dp(target+1, -1);
-        return mem(nums, target, dp);
+        // vector<int> dp(target+1, -1);
+        // return mem(nums, target, dp);
+        return tab(nums, target);
     }
 };
