4be64c9835c85f5472b4eae90e907930362578b8
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-08-16 00:01:53 +0530

    Time: 275 ms (57.14%), Space: 109.9 MB (42.86%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     int edgeScore(vector<int>& edges) {
-        int res = -1,count = -1;
-        // int[] arr = new int[edges.length];
-        vector<long long>arr(edges.size());
-        for(int i=0;i<edges.size();i++){
-            arr[edges[i]] += i;
-            if(arr[edges[i]] > count){
-                res = edges[i];
-                count = arr[edges[i]];
-            }else if(arr[edges[i]] == count) res = min(res,edges[i]);
+
+        int ans = -1, ct = -1;
+        vector<long long> v(edges.size());
+        for(int i = 0; i < edges.size(); i++){
+            v[edges[i]] += i;
+            if(v[edges[i]] > ct){
+                ct = v[edges[i]];
+                ans = edges[i];
+            }
+            else if(v[edges[i]] == ct){
+                ans = min(ans, edges[i]);
+            }
         }
-        
-        return res;
-        
-        // int n = edges.size();
-        // int ans = -1, ct = -1;
-        // int v[n] = {0};
-        // for(int i = 0; i < edges.size(); i++){
-        //     v[edges[i]] += i;
-        //     if(v[edges[i]] > ct){
-        //         ct = v[edges[i]];
-        //         ans = edges[i];
-        //     }
-        //     else if(v[edges[i]] == ct){
-        //         ans = min(ans, edges[i]);
-        //     }
-        // }
-        // return ans;
+        return ans;
     }
 };
