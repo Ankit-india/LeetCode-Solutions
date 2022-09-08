457f459fd3cf0adf1a8719a9207bc8f75145950d
Ankit Kumar <71247375+Ankit-india@users.noreply.github.com>
2022-09-08 22:56:37 +0530

    Time: 3 ms (79.26%), Space: 7.6 MB (46.37%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,4 +1,11 @@
 class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return 1;
+    }
+};
+/*
+class Solution {
     int help(int i,int j,vector<int>& piles,vector<vector<int>>&dp){
         if(i>j) return 0;
         int takestart=0,takeend=0;
@@ -10,6 +17,14 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         vector<vector<int>>dp(piles.size()+1,vector<int>(piles.size()+1,-1));
-        return help(0,piles.size()-1,piles,dp)>0?true:false;
+        int ans = help(0,piles.size()-1,piles,dp);
+        for(int i = 0; i < piles.size()+1; i++){
+            for(int j = 0; j < piles.size()+1; j++){
+                cout << dp[i][j] << " ";
+            }
+            cout << endl;
+        }
+        return ans > 0;
     }
 };
+*/
